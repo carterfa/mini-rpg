865f81a93b24178d12d6f9703574f4d4b2805163
Farrah Carter <carterfarraha@gmail.com>
2019-10-18 22:01:09 -0400

    Added all enemies defeated state.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ let characterSelected = false;
 let targetSelected = false;
 let playerChoice = "";
 let targetChoice = "";
+let enemyCount = 3;
 
 let player = {
     hp: 100,
@@ -17,75 +18,98 @@ let target = {
 
 const game = {
 
+    loadPlayer: function () {
+        if (playerChoice === "fighter") {
+
+            player.hp = 200;
+            player.attack = 20;
+
+        } else if (playerChoice === "knight") {
+            player.hp = 175;
+            player.attack = 25;
+
+        } else if (playerChoice === "mage") {
+            player.hp = 100;
+            player.attack = 5;
+
+        } else if (playerChoice === "ranger") {
+            player.hp = 75;
+            player.attack = 5;
+
+        }
+    },
+
+    loadTarget: function () {
+        if (targetChoice === "fighter") {
+
+            target.hp = 200;
+            target.attack = 20;
+
+        } else if (targetChoice === "knight") {
+            target.hp = 175;
+            target.attack = 25;
+
+        } else if (target.Choice === "mage") {
+            target.hp = 100;
+            target.attack = 5;
+
+        } else if (target.Choice === "ranger") {
+            target.hp = 75;
+            target.attack = 5;
+
+        }
+    },
+
     attack: function () {
         target.hp = target.hp - player.attack;
         player.hp = player.hp - target.counter;
-        console.log(target.hp);
-        console.log(player.hp);
+        console.log("Player HP " + player.hp);
+        console.log("Target HP " + target.hp)
 
     },
 
     check: function () {
+
         if (player.hp <= 0) {
             console.log("GAME OVER");
         } else if (target.hp <= 0) {
-            console.log("YOU WIN");
+            console.log("DEFEATED ENEMY");
+            let imageID = "#" + targetChoice;
+            $(imageID).remove();
+            targetSelected = false;
+            enemyCount--;
+            console.log(enemyCount);
+            if (enemyCount == 0){
+                console.log("ALL ENEMIES DEFEATED!");
+            }
         }
 
     }
 
 }
 
-$("#attackBtn").on("click", function () {
-    if ((characterSelected = true) && (targetSelected = true)) {
-        game.attack();
-        game.check();
-    }
+$(document).ready(function () {
+    $("#attackBtn").on("click", function () {
+        if ((characterSelected == true) && (targetSelected == true)) {
+            
+            game.attack();
+            game.check();
+        }
 
+    })
+
+    $(".character").on("click", function () {
+        if (characterSelected == false) {
+            playerChoice = $(this).attr("id");
+            characterSelected = true;
+            $(this).appendTo("#playerZone");
+            game.loadPlayer();
+        } else if (targetSelected == false) {
+            targetSelected = true;
+            targetChoice = $(this).attr("id");
+            $(this).appendTo("#enemyZone");
+            game.loadTarget();
+        }
+
+    })
 })
-
-$(".character").on("click", function () {
-    if (characterSelected == false) {
-        playerChoice = $(this).attr("id");
-        characterSelected = true;
-        $(this).appendTo("#playerZone");
-    } else if (targetSelected == false) {
-        targetSelected = true;
-        targetChoice = $(this).attr("id");
-        $(this).appendTo("#enemyZone");
-    }
-
-})
-
-
-let fighterStats = {
-    hp: 100,
-    attack: 10,
-    counter: 5,
-    player: false,
-    targeted: false
-};
-
-let knightStats = {
-    hp: 100,
-    attack: 10,
-    counter: 5,
-    player: false,
-    targeted: false
-};
-
-let darkMageStats = {
-    hp: 100,
-    attack: 10,
-    counter: 5,
-    player: false,
-    targeted: false
-};
-
-let rangerStats = {
-    hp: 100,
-    attack: 10,
-    counter: 5,
-    player: false,
-    targeted: false
-};
